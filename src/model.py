d80257d706293f37fe76fd39e2ec1d574b767ebf
mongsam2 <songjaehyun121476@gmail.com>
2025-05-13 13:20:14 +0900

    feat: 프롬프트 예시 수정, 답변 본문 확인

--- src/model.py
+++ src/model.py
@@ -98,6 +98,7 @@ class LlamaModel:
         )
 
     def __extract_answer(self, text):
+        print(text)
         result = re.findall(r"\[출력시작\]\s*(답변:\s*.*)\s*\[출력끝\]", text)
         raw_answer = result[-1]  # 프롬프트를 제외한 답변만 추출
         result = re.findall(r"답변: .*\s*", raw_answer)[-1].split("답변: ")[-1]
